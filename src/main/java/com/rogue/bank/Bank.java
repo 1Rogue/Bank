baefbbce911aeba896bc9c7f8c6aea40390cf2c0
Spencer Alderman <spencer@1rogue.com>
2013-11-21 12:39:51 -0500

    init commit

---
+++ src/main/java/com/rogue/bank/Bank.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2013 Spencer Alderman
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.rogue.bank;
+
+/**
+ * Main class for Bank
+ *
+ * @since 1.0.0
+ * @author Spencer Alderman
+ * @version 1.0.0
+ */
+public class Bank {
+
+    /**
+     * Initial method
+     * 
+     * @since 1.0.0
+     * @version 1.0.0
+     * 
+     * @param args Command-line arguments (Unused)
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
